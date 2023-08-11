825c172c8437fb1f0787ced7df07b3ba7a7ef0ad
HaroldOlaya <haroldolaya.2499@hotmail.com>
2023-08-11 14:01:32 -0500

    funciones con teclado

--- main.py
+++ main.py
@@ -13,7 +13,12 @@ screen = pygame.display.set_mode((800, 500))
 clock =pygame.time.Clock()
 # eliminar la visivilidad del mouse
 pygame.mouse.set_visible(0)
-
+# CORDENADAS DEL OBJETO
+coord_x=10
+coord_y=10
+# Velocidad
+x_speed =0
+y_speed =0
 # bucle para que se mantenga la ventana abierta
 while True:
     for event in pygame.event.get():
@@ -21,13 +26,30 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    mouse_post=pygame.mouse.get_pos()
-    print(mouse_post)
-    x=mouse_post[0]
-    y=mouse_post[1]
-    screen.fill(white)
+        # eventos teclado
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_speed= -3
+            if event.key == pygame.K_RIGHT:
+                x_speed=3
+            if event.key == pygame.K_UP:
+                y_speed= -3
+            if event.key == pygame.K_DOWN:
+                y_speed=3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                x_speed=0
+            if event.key == pygame.K_RIGHT:
+                x_speed=0
+            if event.key == pygame.K_UP:
+                y_speed=0
+            if event.key == pygame.K_DOWN:
+                y_speed=0
 
-    pygame.draw.rect(screen,red,(x,y,100,100))
+    screen.fill(white)
+    coord_x += x_speed
+    coord_y += y_speed
+    pygame.draw.rect(screen,red,(coord_x,coord_y,100,100))
 
     pygame.display.flip()
 
